443c9f6e9dfa27b9b18fda1ae0df3216ddef009b
Gustavo <gustavoalzarate@gmail.com>
2025-06-06 22:12:48 -0600

    feat: add modal to save new task

--- src/api/task.ts
+++ src/api/task.ts
@@ -1,4 +1,5 @@
 import { getHeaders } from '@/helpers'
+import type { NewTask } from '@/types'
 
 export const fetchGetTasks = async () => {
   try {
@@ -9,5 +10,19 @@ export const fetchGetTasks = async () => {
     return await response.json()
   } catch (error) {
     console.error(`Ocurrió un error - fetchGetTasks: ${error}`);
+  }
+}
+
+export const fetchSaveTask = async (task: NewTask) => {
+  try {
+    const URL = `${import.meta.env.VITE_BACKEND_URL}/task`
+    const response = await fetch(URL, {
+      method: 'POST',
+      headers: getHeaders(),
+      body: JSON.stringify(task)
+    })
+    return await response.json()
+  } catch (error) {
+    console.error(`Ocurrió un error - fetchSaveTask: ${error}`);
   }
 }
